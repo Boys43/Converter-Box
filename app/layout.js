258072919470f332fc2243bb06eff372f3be228c
Nouman <nt50616849@gmail.com>
2025-11-14 09:48:34 +0500

    Google Analytics Added

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import Navbar from './components/Navbar';
+import Script from "next/script";
 
 export const metadata = {
   title: "ConverterBox - Free YouTube to MP3 Converter Online | Fast & High Quality",
@@ -65,23 +66,23 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link rel="preconnect" href="https://img.youtube.com" />
-        
+
         {/* Optimized font loading */}
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        
+
         {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        
+
         {/* Theme color */}
         <meta name="theme-color" content="#3B82F6" />
-        
+
         {/* Structured Data */}
         <script
           type="application/ld+json"
@@ -113,8 +114,24 @@ export default function RootLayout({ children }) {
       <body className="antialiased font-poppins">
         <Navbar />
         {children}
+
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-60HM2EMYCY"
+        />
+        <Script id="ga-script" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-60HM2EMYCY', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+
         <Analytics />
-        <SpeedInsights/>
+        <SpeedInsights />
       </body>
     </html>
   );
